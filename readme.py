00865a98a27537ba25a09c659578c85276b1ee13
Marko <stefan.micic@toptal.com>
2024-04-17 16:46:24 +0200

    second

--- readme.py
+++ readme.py
@@ -1,22 +1,6 @@
 import pandas as pd
 import os
-
-def create():
-    # Create a dictionary with initial values
-    data = {
-        'PLAYERS': ['Stefan Micic', 'Marko Raseta', 'Marko Vasilic'],
-        'PLAYED': [14, 14, 14],
-        'WINS': [5, 7, 5],
-        'DRAWS': [1, 4, 3],
-        'LOSSES': [8, 3, 6],
-        'SCORED': [18, 33, 26],
-        'CONCEDED': [23, 24, 27],
-        'POINTS': [16, 25, 18]
-    }
-
-    # Create DataFrame
-    df = pd.DataFrame(data, index=[0, 1, 2])
-    df.to_csv("table.csv")
+from create_table import create
 
 def update(df):
     df.drop(columns=["Unnamed: 0"], inplace=True)
